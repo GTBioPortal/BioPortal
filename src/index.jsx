266f00478023e7734f8ad93bac6919d2f2ed204b
Matthew Fraschilla <mfraschilla3@gatech.edu>
2018-11-25 05:39:04 -0500

    remove stash message

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-<<<<<<< Updated upstream
-import App from './components/App'
 
-=======
 import StudentHomepage from './pages/StudentHomepage'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -17,7 +14,6 @@ const router = (
         </Router>
     </Provider>
 );
->>>>>>> Stashed changes
 
 ReactDOM.render(
     router,
