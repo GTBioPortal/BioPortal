e67d259f6118a316288e489a09688ce049f0250f
Matthew Fraschilla <mfraschilla3@gatech.edu>
2019-02-17 17:09:15 -0500

    migrate to MySQL

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,7 @@ class Development(object):
     DEBUG = True
     TESTING = False
     JWT_SECRET_KEY = 'bioportal'
-    SQLALCHEMY_DATABASE_URI = 'postgres://bioportal:bioportal@35.202.231.38/bioportal_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bioportal:bioportal@35.222.53.63:3306/bioportal_db'
 
 
 app_config = {
--- backend/migrations/versions/cf3d842ab091_.py
+++ backend/migrations/versions/cf3d842ab091_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 46593b7585b5
+Revision ID: cf3d842ab091
 Revises: 
-Create Date: 2019-02-15 15:47:56.634411
+Create Date: 2019-02-17 17:07:46.523759
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '46593b7585b5'
+revision = 'cf3d842ab091'
 down_revision = None
 branch_labels = None
 depends_on = None
