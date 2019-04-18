9d2a221dbd0f2edef10d30a4a5c0cf955752cec8
pritshah98 <addison20>
2019-04-17 20:36:57 -0400

    finished view app page

--- src/pages/ViewApplicantsPage.jsx
+++ src/pages/ViewApplicantsPage.jsx
@@ -9,6 +9,8 @@ import EmployerNavbar from '../components/EmployerNavbar';
 import '../styles/job-description.scss';
 import '../styles/card.scss';
 import API from '../api/api'
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
 
 /**
  * JobDescription component shows the description, name, location, deadline, and position for a job posting
@@ -18,10 +20,10 @@ class ViewApplicantsPage extends React.Component {
         super(props)
 
         this.state = {
-            names: []
+            apps: []
         }
 
-        console.log(this.props);
+        // console.log(this.props);
         // const job = this.props.location.data.job;
         var auth_token = localStorage.getItem('employer-token');
         var authorize = 'Bearer ' + auth_token
@@ -34,17 +36,35 @@ class ViewApplicantsPage extends React.Component {
         API.get(url, 
             {headers: headers}
         ).then(res => {
-            console.log(res.data.applications);
-            var temp = []
-             for (let i = 0; i < res.data.applications.length; i++) {
-                if (i === 0) {
-                    temp.push(res.data.applications[i].applicant+ ", ")
-                } else {
-                    temp.push(res.data.applications[i].applicant)
-                }
-            }
-            this.setState({names: temp});
-      });
+            // console.log(res.data.applications);
+            this.setState({apps: res.data.applications});
+        });
+    }
+
+    downloadResume(app) {
+        console.log(app);
+        var auth_token = localStorage.getItem('employer-token');
+        var authorize = 'Bearer ' + auth_token;
+        var url = '/files/' + app.resume;
+        var headers = {
+            'Content-Type': 'application/json',
+            'Authorization': authorize
+        }
+        API.post(url,
+            {application_id: app.id},
+            {responseType: 'blob', 
+            headers: headers}
+        ).then(res => {
+            console.log(res);
+            var blob = new Blob(
+                [res.data],
+                {type: 'application/pdf'});
+            const fileURL = URL.createObjectURL(blob);
+            window.open(fileURL);
+        })
+        .catch(res => {
+            // console.log(res);  
+        });
     }
 
     render() {
@@ -52,8 +72,14 @@ class ViewApplicantsPage extends React.Component {
         	<div>
                 <EmployerNavbar msgCount={0} notificationCount={0}/>
                 <Typography variant="h5" align="center" >
-                    Applicants: {this.state.names}
+                    Applicants
                 </Typography>
+                <br/>
+                <Grid container direction="column" justify="center" alignItems="center">
+                    {this.state.apps.map((el, index) => {
+                        return <Link key={index} align="center" variant="subtitle1" onClick={() => this.downloadResume(el)} gutterBottom>Applicant: {el.applicant} Resume <br/> </Link>
+                    })}
+                </Grid>
         	</div>
 
         )
