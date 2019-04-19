ace6862362b32796a5b852a52c53dc7763c970ed
pritshah98 <addison20>
2019-04-18 22:25:45 -0400

    updated admin home page

--- src/pages/AdminHomePage.jsx
+++ src/pages/AdminHomePage.jsx
@@ -9,6 +9,10 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Snackbar from '@material-ui/core/Snackbar';
 import API from '../api/api'
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 /**
  * Student homepage that contains job cards
@@ -16,13 +20,59 @@ import API from '../api/api'
 class AdminHomePage extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            employers: []
+        }
+
+        var auth_token = localStorage.getItem('admin-token');
+        var authorize = 'Bearer ' + auth_token
+        var headers = {
+            'Content-Type': 'application/json',
+            'Authorization': authorize
+        }
+        API.get('admin/employers', {headers: headers}).then(res => {
+            // console.log(res);
+            var employersSort = res.data.employers;
+            employersSort.sort((a,b) => a.is_approved - b.is_approved);
+            // console.log(employersSort);
+            this.setState({ employers: employersSort });
+        });
+    }
+
+    verifyEmployer(employer) {
+        var auth_token = localStorage.getItem('admin-token');
+        var authorize = 'Bearer ' + auth_token
+        var headers = {
+            'Content-Type': 'application/json',
+            'Authorization': authorize
+        }
+        var url = '/employer/' + employer.id + '/verify';
+        API.get(url, {headers: headers}).then(res => {
+            // console.log(res);
+            window.location.reload();
+        });
     }
 
     render() {
         return (
             <div>
                 <AdminNavbar />
-                ADMIN HOMEPAGE
+                <Grid container direction="column" justify="center" alignItems="center">
+                    {this.state.employers.map((el, index) => {
+                        return (
+                            <React.Fragment key={index}>
+                                <Grid container direction="row" justify="center" alignItems="center">
+                                    <Typography align="center" variant="subtitle1" gutterBottom> Company: {el.company} / Email: {el.email} / Verified: {Boolean(el.is_approved).toString()} </Typography>
+                                    {!el.is_approved &&
+                                    <Button
+                                        size="small"
+                                        variant="outlined"
+                                        onClick = {() => this.verifyEmployer(el)}> Verify</Button>}
+                                    </Grid>
+                            </React.Fragment>)
+                    })}
+                </Grid>
             </div>
         );
     }
